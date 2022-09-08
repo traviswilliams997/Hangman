c0bcc3a309fcd93bca28e041b873ef11a6164be7
Travis Williams <traviswilliams997@gmail.com>
2022-09-08 00:04:18 -0500

    Create function for single game round

--- main.rb
+++ main.rb
@@ -23,15 +23,19 @@ def check_letter_guess(array, word, guess)
     end
 
 end
+def game_round(guess_array, random_word)
+    puts "Enter guess"
+    player_choice = gets.chomp
+    check_letter_guess(guess_array, random_word, player_choice)
+    
+    
+end
 words = File.readlines('google-10000-english-no-swears.txt')
 correct_length_words = Array.new
 correct_length_of_words(correct_length_words, words)
 random_word = choose_random_word(correct_length_words)
  puts random_word 
 
-  
 guess_array = Array.new(random_word.length, "_")
-player_choice = "B"
-check_letter_guess(guess_array, random_word, player_choice)
-
+game_round(guess_array, random_word)
 p guess_array
