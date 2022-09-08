935127cc4f8d3e38077831b670e31d78c7ece7da
Travis Williams <traviswilliams997@gmail.com>
2022-09-07 23:34:04 -0500

    Edit choose random word function

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 puts "Hangman!"
 
+def choose_random_word(words)
+    
+   random_word =  words.sample
+   random_word
+end
+
 def correct_length_of_words(new_array, word_list)
     word_list.each do |word|
         if word.length > 5 && word.length < 13
@@ -14,35 +20,10 @@ words = File.readlines('google-10000-english-no-swears.txt')
 correct_length_words = Array.new
 
 correct_length_of_words(correct_length_words, words)
-puts correct_length_words.length
-
+puts choose_random_word(correct_length_words)
   
 
   
-# puts choose_random_word(words) 
 
 
 
-# def choose_random_word(word)
-#     begin
-#    random_word =  word.sample
-#    check = true
-#    while check
-
-#     if  random_word.length >= 5 && random_word.length <= 12 
-#         check = false
-#     else
-#         count = count + 1
-#         if count > 1000
-#             exit 
-#         end
-#         choose_random_word(word)
-        
-        
-#     end
-# end
-# rescue
-#     choose_random_word(word)
-# end
-#     random_word
-# end
