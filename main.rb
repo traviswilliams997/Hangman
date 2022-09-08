fcea7c642498bcb011474b36f0220dba4e85cf43
Travis Williams <traviswilliams997@gmail.com>
2022-09-07 23:31:28 -0500

    Create function to remove words of incorrect length from document

--- main.rb
+++ main.rb
@@ -1,16 +1,48 @@
 puts "Hangman!"
-def choose_random_word(word)
-   random_word =  word.sample
-   check = true
-   while check
 
-    if  random_word.length >= 5 && random_word.length <= 12 
-        check = false
-    else
-        choose_random_word(word)
-    end
+def correct_length_of_words(new_array, word_list)
+    word_list.each do |word|
+        if word.length > 5 && word.length < 13
+           new_array << word
+        end
+       
+     end
+   
 end
-    random_word
-end
+
 words = File.readlines('google-10000-english-no-swears.txt')
-puts choose_random_word(words) 
+correct_length_words = Array.new
+
+correct_length_of_words(correct_length_words, words)
+puts correct_length_words.length
+
+  
+
+  
+# puts choose_random_word(words) 
+
+
+
+# def choose_random_word(word)
+#     begin
+#    random_word =  word.sample
+#    check = true
+#    while check
+
+#     if  random_word.length >= 5 && random_word.length <= 12 
+#         check = false
+#     else
+#         count = count + 1
+#         if count > 1000
+#             exit 
+#         end
+#         choose_random_word(word)
+        
+        
+#     end
+# end
+# rescue
+#     choose_random_word(word)
+# end
+#     random_word
+# end
