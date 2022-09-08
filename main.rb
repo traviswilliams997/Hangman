6f30029fbbb3f0b2c1fede255b5e699be8410795
Travis Williams <traviswilliams997@gmail.com>
2022-09-07 23:08:18 -0500

    Create function to choose word

--- main.rb
+++ main.rb
@@ -1,4 +1,16 @@
 puts "Hangman!"
+def choose_random_word(word)
+   random_word =  word.sample
+   check = true
+   while check
 
-contents = File.readlines('google-10000-english-no-swears.txt')
-puts contents[0]
+    if  random_word.length >= 5 && random_word.length <= 12 
+        check = false
+    else
+        choose_random_word(word)
+    end
+end
+    random_word
+end
+words = File.readlines('google-10000-english-no-swears.txt')
+puts choose_random_word(words) 
